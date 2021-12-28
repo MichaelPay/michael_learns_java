a481f74b9bc5a87c78000dd8aa852d56daa1c291
Michael Pay <michael.pay@mail.com>
2021-12-28 13:57:51 -0800

    Michael Learns Functional Programming in Java

--- lcc-java-fp-printer-1.1.0/src/main/java/com/teamtreehouse/challenges/Printer.java
+++ lcc-java-fp-printer-1.1.0/src/main/java/com/teamtreehouse/challenges/Printer.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.challenges;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Printer {
 
@@ -21,7 +22,7 @@ public class Printer {
 
   public static void printChipmunksDeclaratively(List<String> chipmunks) {
     // TODO: Print out each of the chipmunks using the forEach method on Iterable
-
+    chipmunks.forEach(s -> System.out.printf("%s! %n", s));
   }
 
 
